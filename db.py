885aa8f3231d3319bfe788ae10722b958102d858
dingcsw <dingcsw@gmail.com>
2018-01-20 11:58:19 +0800

    new function: print task

--- db.py
+++ db.py
@@ -24,12 +24,25 @@ def list_all_tasks():
             i, task['state']
         ))
 
+def print_task(i):
+    db = get_db().get()
+    if not i in range(len(db)):
+        print('Task {} does not exists.'.format(i))
+        return
+    task = db[i]
+    print('Task ID       {}'.format(i))
+    print('Task status   {}'.format(task['state']))
+    if 'metrics' in task.keys():
+        print('Eval metrics  {}'.format(task['metrics']))
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Access to firebase database')
     parser.add_argument('--reset', action='store_true',
         help='reset database')
     parser.add_argument('--state', action='store_true',
         help='list all tasks')
+    parser.add_argument('-t', type=int,
+        help='get status of specific task')
     args = parser.parse_args()
 
     if args.reset:
@@ -37,4 +50,7 @@ if __name__ == '__main__':
 
     if args.state:
         list_all_tasks()
+
+    if args.t:
+        print_task(args.t)
         
